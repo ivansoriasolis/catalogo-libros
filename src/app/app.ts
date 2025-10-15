d84f4005138e4b34ab8b1c80ae4ae67771bea68e
ivansoriasolis <soria.solis.ivan@googlemail.com>
2025-10-14 21:23:25 -0500

    todo funcionando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,15 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecera } from './cabecera/cabecera';
 import { LibroLista } from './libro-lista/libro-lista';
 import { FormsModule } from '@angular/forms';
-import { LibroDetalle } from './libro-detalle/libro-detalle';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecera, LibroLista, LibroDetalle, FormsModule, CommonModule],
+  imports: [Cabecera, LibroLista, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('catologo-libros');
-
-  libroSeleccionado: any = null;
-
 }
--- src/app/libro-lista/libro-lista.ts
+++ src/app/libro-lista/libro-lista.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { LibroDetalle } from '../libro-detalle/libro-detalle';
 import { LibroServicio } from '../services/libro-servicio';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-libro-lista',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, LibroDetalle],
   templateUrl: './libro-lista.html',
   styleUrl: './libro-lista.css'
 })
 export class LibroLista {
-  @Output() seleccionado = new EventEmitter<any>();
 
   libroSeleccionado: any = null;
   verDetalle: boolean = false;
@@ -18,12 +17,7 @@ export class LibroLista {
 
   libros: any[] = [];
 
-  constructor(private libroServicio: LibroServicio) { }
-
-  onVerDetalle() {
-    this.verDetalle = true;
-    this.seleccionado.emit(this.libroSeleccionado);
-  }
+  constructor(private libroServicio:LibroServicio) {}
 
   ngOnInit() {
     this.libros = this.libroServicio.getLibros();
@@ -34,5 +28,7 @@ export class LibroLista {
     this.verDetalle = false;
   }
 
-
+  onVerDetalle() {
+    this.verDetalle = true;
+  }
 }
