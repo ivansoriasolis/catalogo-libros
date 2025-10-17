4848467e54c70f8a17dc7e439e4d36e3eb43a318
ivansoriasolis <soria.solis.ivan@googlemail.com>
2025-10-16 20:23:41 -0500

    fase1: con comentarios

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Cabecera } from './cabecera/cabecera';
-import { LibroLista } from './libro-lista/libro-lista';
+import { Cabecera } from './cabecera/cabecera'; // Importa el componente Cabecera
+import { LibroLista } from './libro-lista/libro-lista'; // Importa el componente LibroLista
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecera, LibroLista],
+  imports: [Cabecera, LibroLista], // Importa los componentes Cabecera y LibroLista
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/libro-lista/libro-lista.ts
+++ src/app/libro-lista/libro-lista.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-libro-lista',
-  imports: [CommonModule],
+  imports: [CommonModule], // Importa CommonModule para usar directivas comunes como *ngFor
   templateUrl: './libro-lista.html',
   styleUrl: './libro-lista.css'
 })
 export class LibroLista {
-  libros = [
-    { titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', anio: 1967, fechaPublicacion: new Date(1967, 5, 5)   },
+  libros = [ // Array de libros
+    { titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', anio: 1967, fechaPublicacion: new Date(1967, 5, 5)   }, 
     { titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', anio: 1605, fechaPublicacion: new Date(1605, 0, 16) },
     { titulo: 'La sombra del viento', autor: 'Carlos Ruiz Zafón', anio: 2001, fechaPublicacion: new Date(2001, 3, 1) },
   ];
