aa152276653f25cb92569fe26655fb4d4e33c2c5
ivansoriasolis <soria.solis.ivan@googlemail.com>
2025-10-16 23:11:21 -0500

    fase8:con comentarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
-import { LibroLista } from './libro-lista/libro-lista';
 import { LibroDetalle } from './libro-detalle/libro-detalle';
 import { About } from './about/about';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/catalogo', pathMatch: 'full' },
     {
+        // Lazy loading del componente Catalogo
         path: 'catalogo',
-        loadComponent: () => import('./catalogo/catalogo').then(m => m.Catalogo),
+        loadComponent: () => import('./catalogo/catalogo').then(m => m.Catalogo), // Componente Catalogo cargado de forma perezosa
         children: [
             {
-                path: 'detalle/:id',
-                loadComponent: () => import('./libro-detalle/libro-detalle').then(m => m.LibroDetalle)
+                // Ruta hija para el detalle del libro
+                path: 'detalle/:id', // Ruta para el detalle del libro
+                loadComponent: () => import('./libro-detalle/libro-detalle').then(m => m.LibroDetalle) // Componente LibroDetalle cargado de forma perezosa
             }
         ]
     },
