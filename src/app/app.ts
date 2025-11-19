02831d88b6542915ce5c7d28579b6fbf7ae35ade
ivansoriasolis <soria.solis.ivan@googlemail.com>
2025-11-18 22:15:39 -0500

    con verificación de autenticacion al iniciar la aplicacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, Input, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Cabecera } from './cabecera/cabecera';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Auth } from '@angular/fire/auth';
+import { AuthServicio } from './servicios/auth-servicio';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,24 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('catologo-libros');
+  authVerificado = false; // Nueva variable para rastrear la verificación de autenticación
 
   libroSeleccionado: any = null;
 
+  constructor(private authServicio: AuthServicio,
+    private router: Router,
+  ){}
+
+  // Verifica el estado de autenticación al iniciar la aplicación
+  ngOnInit():void{
+    this.authServicio.estadoAuth$.subscribe((usuario) => {
+      this.authVerificado = true; // Marca que la verificación ha sido completada
+      if (usuario) { // Si el usuario está autenticado
+        this.router.navigate(['/cataologo']);
+      }  
+      else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
